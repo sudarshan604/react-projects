55f3fa43750f0dc181a0e470df8122be8ac5fe20
sudarshan kshetri <sudarshandn1999@gmail.com>
2022-11-15 22:19:52 +0545

    adding axios

---
+++ axios-tutorial-react-main/src/App.js
@@ -0,0 +1,13 @@
+import Title from "./components/Title";
+import Setup from "./examples/3-post-request";
+
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup />
+    </main>
+  );
+}
+
+export default App;
