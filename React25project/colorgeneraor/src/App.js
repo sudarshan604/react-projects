d8a62cf1bb0107d7d95de7e7423f9ace455a68f5
sudarshan kshetri <sudarshandn1999@gmail.com>
2022-11-16 21:09:53 +0545

    color generator

---
+++ React25project/colorgeneraor/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+
+import Values from "values.js";
+
+function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handlSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handlSubmit}>
+          <input
+            type="text"
+            value={color}
+            placeholder="#f15025"
+            onChange={(e) => setColor(e.target.value)}
+            className={`${error ? "error" : null}`}
+          />
+
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>list goueshere</h4>
+      </section>
+    </>
+  );
+}
+
+export default App;
