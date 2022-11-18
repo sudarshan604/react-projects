f6221c3f5cc13e37e55b63484fc855804f55ee88
sudarshan kshetri <sudarshandn1999@gmail.com>
2022-11-18 20:33:14 +0545

    adding gocery

---
+++ React25project/grocery/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+function App() {
+  const [name, setName] = useState();
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditId] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      //display alert
+    } else if (name && isEditing) {
+      //deal with edit
+    }
+  };
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            placeholder="e.g eggg"
+            typr="text"
+            className="grocery"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clearitems</button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
