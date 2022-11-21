557bde57b6a58bc27b5e3acfecbca1664499232e
sudarshan kshetri <sudarshandn1999@gmail.com>
2022-11-21 14:41:35 +0545

    adding sidebar

---
+++ React25project/sidebar/src/Modal.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalContext();
+
+  return (
+    <div
+      className={`${
+        isModalOpen ? "show-modal modal-overlay " : "modal-overlay "
+      }`}
+    >
+      <div className="modal-container">
+        <h3>modal</h3>
+        <button className="close-modal-btn" onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
---
+++ React25project/sidebar/src/Sidebar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+import { useGlobalContext } from "./context";
+
+const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar "}`}>
+      <div className="sidebar-header">
+        <img src={logo} className="logo" alt="coding-addict" />
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.url}>
+                {link.icon}
+                {link.text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-icons">
+        {social.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.url}>{link.icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
