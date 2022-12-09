2f7039680e6c6478840f0a73ac14520cf70f7eb9
sudarshan kshetri <sudarshandn1999@gmail.com>
2022-12-09 19:08:56 +0545

    building redux from scratch

--- Redux/redux-starter/src/index.js
+++ Redux/redux-starter/src/index.js
@@ -1,22 +1,29 @@
-import store from "./store";
-import * as actions from "./actionType";
+// import store from "./store";
+// import * as actions from "./actionType";
 
-import { bugAdded, bugResolved } from "./action";
+// import { bugAdded, bugResolved } from "./action";
 
-const unscribe = store.subscribe(() => {
-  console.log("store changed", store.getState());
-});
+// const unscribe = store.subscribe(() => {
+//   console.log("store changed", store.getState());
+// });
 
-store.dispatch(bugAdded("bug1"));
-unscribe();
+// store.dispatch(bugAdded("bug1"));
+// unscribe();
 
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-});
+// store.dispatch({
+//   type: actions.BUG_REMOVED,
+//   payload: {
+//     id: 1,
+//   },
+// });
 
-store.dispatch(bugResolved(1));
+// store.dispatch(bugResolved(1));
 
-console.log(store.getState());
+// console.log(store.getState());
+
+import store from "./CustomStore";
+import * as actions from "./action";
+
+store.subscribe(() => {});
+
+store.dispatch(actions.bugAdded("bug1"));
