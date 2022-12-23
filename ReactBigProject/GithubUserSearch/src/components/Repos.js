e4e8cb81af645ac997c215a1dca7dfb055244304
sudarshan kshetri <sudarshandn1999@gmail.com>
2022-12-23 17:51:46 +0545

    fusion charts complete

--- ReactBigProject/GithubUserSearch/src/components/Repos.js
+++ ReactBigProject/GithubUserSearch/src/components/Repos.js
@@ -5,36 +5,65 @@ import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   
+ 
+
+
   const {githubRepo}=React.useContext(GithubContext)
+
 
   let languages=githubRepo.reduce((acc,current)=>{
     
-    const {language}=current
-    
-    if(!language) return  acc
+    const {language,stargazers_count}=current
+  if(!language) return  acc
     if(!acc[language]) {
-      acc[language]={label:language,value:1}
+      acc[language]={label:language,value:1,stars:stargazers_count}
     }
         else 
         {
-          acc[language]={...acc[language],value:acc[language].value+1}
+          acc[language]={...acc[language],value:acc[language].value+1,
+            stars:acc[language].stars+stargazers_count
+          }
+      
         }
     return acc
 
   },{})
 
-  languages=Object.values(languages).sort((a,b)=>{
+  const mostUsed=Object.values(languages).sort((a,b)=>{
      return b.value-a.value
   }).slice(0,5)
 
+//most star
+const mostPopular=Object.values(languages).sort((a,b)=>{
+ return b.stars-a.stars
+}).map(item=>{
+  return {...item,value:item.stars}
+}).slice(0,5)
+ 
 
-console.log(languages)
+//stars,forks
+let {stars,forks}=githubRepo.reduce((acc,item)=>{
+  const {stargazers_count,name,forks}=item
+     acc.stars[stargazers_count]={label:name,value:stargazers_count}
+     acc.forks[forks]={label:name,value:forks}     
+    return acc
+},{
+  stars:{},
+  forks:{}
+})
+
+const repoStar=Object.values(stars).slice(-5).reverse()
+
+const repoFork=Object.values(forks).slice(-5).reverse()
+console.log(repoFork)
+
   return <section className='section'>
     <Wrapper className='section-center'>
-      <Pie3D  data={languages}/>
-     
-      {/* <ExampleChart data={chartData}/>---> for my reference */}
- </Wrapper>
+      <Pie3D  data={mostUsed}/>
+       <Column3D data={repoStar}/>
+     <Doughnut2D data={mostPopular}/>   
+  <Bar3D  data={repoFork}/>
+   </Wrapper>
   </section>
 
 
