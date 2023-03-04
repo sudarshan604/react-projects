39c8b20917432a188b09e6d69fccc195a71ea015
sudarshan <sudarshandn1999@gmail.com>
2023-03-04 15:18:08 +0545

    starting typescript udemy

---
+++ typescript/typescriptudemy/get starting/dist/2-boolean-string-number.js
@@ -0,0 +1,26 @@
+"use strict";
+let numbers = [1, 2, 3, 4, 5];
+let products = ["hats", "shirst", "shirt"];
+let mixDdata = [4, 'hats'];
+let myNums = [
+    [1, 2],
+    [2, 3]
+];
+let myString = [
+    ["hi"],
+    ["hello"]
+];
+let myMixedNested = [
+    ["hwllo", 1]
+];
+const courseDetailes = ["hello", 12];
+const courseDetailes1 = ["hello", 12];
+var RollNumber;
+(function (RollNumber) {
+    RollNumber[RollNumber["John"] = 1] = "John";
+    RollNumber[RollNumber["Kelly"] = 2] = "Kelly";
+    RollNumber[RollNumber["sandra"] = 3] = "sandra";
+    RollNumber[RollNumber["Joseph"] = 4] = "Joseph";
+    RollNumber[RollNumber["Samantha"] = 5] = "Samantha";
+})(RollNumber || (RollNumber = {}));
+let studentRollNumber = RollNumber.sandra;
---
+++ typescript/typescriptudemy/get starting/src/2-boolean-string-number.ts
@@ -0,0 +1,76 @@
+let numbers:number[]=[1,2,3,4,5]
+
+//array of string
+let products:string[]=["hats","shirst","shirt"]
+
+//Array of string and numbers
+let mixDdata:(string | number)[]=[4,'hats']
+
+//nested number rype array
+let myNums:number[][]=[
+      [1,2],
+      [2,3]
+    ]
+
+//nested number rype string
+ let myString:string[][]=[
+    ["hi"],
+    ["hello"]
+  ]
+
+
+let myMixedNested:(string|number)[][]=[
+    ["hwllo",1]
+]
+
+
+
+
+
+
+//tuples
+const courseDetailes:[string,number]=["hello",12]
+
+const courseDetailes1=["hello",12]
+
+
+
+//Enum
+
+//1.Numneric enum
+
+// enum RollNumber{
+//   John,
+//   Kelly,
+//   sandra,
+//   Joseph,
+//   Samantha
+
+// }
+
+enum RollNumber{
+  John=1,
+  Kelly,
+  sandra,
+  Joseph,
+  Samantha
+
+}
+let studentRollNumber:RollNumber=RollNumber.sandra
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
