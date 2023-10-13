5f37a08ef71f838a5ad36c309e2271c1684b5c9e
sudarshan604 <kshetri.sudarshann@gmail.com>
2023-10-13 10:03:27 -0700

    prisma and database connect

--- moshNextjs/next-app/app/api/users/[id]/route.tsx
+++ moshNextjs/next-app/app/api/users/[id]/route.tsx
@@ -1,14 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
 
 
-
-export function GET(request:NextRequest,
-     {params}:{params:{id:number}}
+export async function GET(request:NextRequest,
+     {params}:{params:{id:string}}
     ){
-if(params.id>10)
+
+ const user=   await prisma.user.findUnique({
+             where:{id: parseInt(params.id)}
+         })
+if(!user)
     return NextResponse.json({error:"user not foinr"},{status:404})
  
 
-return NextResponse.json({id:1,name:'mosh'})    
+return NextResponse.json(user)    
 
 }
--- moshNextjs/next-app/app/api/users/route.tsx
+++ moshNextjs/next-app/app/api/users/route.tsx
@@ -1,31 +1,44 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
+import prisma from "@/prisma/client";
+export async function GET(request:NextRequest){
 
-export function GET(request:NextRequest){
-
-    return NextResponse.json([
-        {
-            "id":1,
-            nameL:'mosh'
-        },
-        {
-            "id":2,
-            nameL:'John'
-        } 
-    ])
+ const users =await prisma.user.findMany()     
+  
+  return NextResponse.json(users)
 }
 
 
 export async function POST(request:NextRequest){
   const body=  await request.json()
  
-  if(!body.name){
-     return NextResponse.json({error:'name is requires'},{status:400})
+const validation= schema.safeParse(body)
+
+  if(!validation.success){
+     return NextResponse.json(validation.error.errors,{status:400})
   }
 
+const user= await prisma.user.findUnique({
+    where:{email:body.email}
+})
 
- return NextResponse.json({id:1,name:body.name},{status:201})
+if(user){
+     return NextResponse.json({error:'user already exit'},{status:400})
+}
+
+const Newuser= await prisma.user.create({
+    data:{
+        name:body.name,
+        email:body.email
+    }
+})
+
+ return NextResponse.json({Newuser},{status:201})
 
 }
+
+
+
 export async function PUT(request:NextRequest,
     {params}:{params:{id:number}} 
     ){
@@ -34,7 +47,23 @@ if(!body.name){
      return NextResponse.json({error:'name is requires'},{status:404})
 }
 
+if(params.id>10)
+{
+    return NextResponse.json({error:'User not found'},{status:404})
+}
+return NextResponse.json({id:1,name:body.name})
+
+}
 
 
+
+export async function DELETE(request:NextRequest,
+    {params}:{params:{id:number}} ){
+
+if(params.id>10)
+    return NextResponse.json({error:'User not found'},{status:404})
+
+return NextResponse.json({})
+    
 
 }
